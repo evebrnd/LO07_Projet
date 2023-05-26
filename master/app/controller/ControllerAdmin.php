4ae272e9621c461e95931bf54e303a375be037c6
Capucine <capucine.room@utt.fr>
2023-05-26 18:52:01 +0200

    controller admin

--- master/app/controller/ControllerAdmin.php
+++ master/app/controller/ControllerAdmin.php
@@ -3,7 +3,28 @@
 require_once '../model/ModelPersonne.php';
 
 class ControllerAdmin {
+    
+    // --- Liste des specialités
+    public static function adminReadAll() {
+        $results = ModelVin::getAll();
+        // ----- Construction chemin de la vue
+        include 'config.php';
+        $vue = $root . '/app/view/admin/view?.php';           //faut créer la vue
+        if (DEBUG)
+         echo ("ControllerAdmin : adminReadAll : vue = $vue");
+        require ($vue);
+    }
+    
+     // Affiche une spécialité particuliere (id)
+    public static function adminReadOne() {
+        $personne_id = $_GET['id'];
+        $results = ModelPersonne::getOne($personne_id);
 
+        // ----- Construction chemin de la vue
+        include 'config.php';
+        $vue = $root . '/app/view/admin/view?.php';          //faut créer la vue
+        require ($vue);
+    }
 
 }
 ?>
